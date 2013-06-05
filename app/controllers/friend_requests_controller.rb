e39035b920ac927eb03ed4543a049905437b1457
Ekramvis <ekramvis@gmail.com>
2013-06-05 15:39:25 -0400

    Friend Request sends safely

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -2,7 +2,8 @@ class FriendRequestsController < ApplicationController
 	before_filter :authenticate_user!
 
 	def create 
-		@friend_request = FriendRequest.new(params[:friend_request])
+		p params
+		@friend_request = FriendRequest.new(user_id: current_user.id, requestee_id: params[:id]) 
 
 		if @friend_request.save
 			render partial: "friend_requests/request"
