16fe7d90fd7ac32ae33695087b8ad2fac65d2de4
Scott and Cody Z <scott+codyz@appacademy.io>
2013-06-07 16:14:51 -0400

    finally back on track

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,13 @@ Friendface::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => friendface_dev,
+      :access_key_id => AWSAccessKeyId,
+      :secret_access_key => AWSSecretKey
+    }
+  }
 end
