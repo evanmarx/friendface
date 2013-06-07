83c4cb3e3eb1abea39b8d31749f4dc917e4c9fb0
Scott and Cody Z <scott+codyz@appacademy.io>
2013-06-07 15:44:47 -0400

    cover up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,4 @@ Friendface::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => friendface_dev,
-      :access_key_id => AKIAIRHP6KPUTGIEH6AA,
-      :secret_access_key => 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
-    }
-  }
 end
